4c17a69249ae489151c53bb11b20683c51f3ce02
Jason Stevens <jay@wizardofthenet.com>
2019-08-08 05:53:01 -0700

    change immortals to not be gods on load

--- src/players.c
+++ src/players.c
@@ -470,8 +470,8 @@ int load_char(const char *name, struct char_data *ch) {
 
     affect_total(ch);
 
-    /* initialization for imms */
-    if (GET_LEVEL(ch) >= LVL_IMMORT) {
+    /* initialization for gods */
+    if (GET_LEVEL(ch) >= LVL_GOD) {
         for (i = 1; i <= MAX_SKILLS; i++)
             GET_SKILL(ch, i) = 100;
         GET_COND(ch, HUNGER) = -1;
